0ab20a9edcb5b12043a152964baafe60fb340853
Prabhanjana Kumar <prabhank@synamedia.com>
2025-02-27 09:40:47 +0530

    Add color property and text layout support to CustomText class

--- customtext.cpp
+++ customtext.cpp
@@ -1,11 +1,20 @@
+#if 0
 #include "customtext.h"
-#include <QQuickTextNode>
+#include <private/qquicktextnode_p.h>
 #include <QFont>
+#include <QSGNode>
 
 CustomText::CustomText(QQuickItem *parent)
     : QQuickItem(parent)
+    , m_layout(nullptr)
+    , m_color(Qt::black)  // Initialize with default color
 {
     setFlag(ItemHasContents, true);
+}
+
+CustomText::~CustomText()
+{
+    delete m_layout;
 }
 
 void CustomText::setText(const QString &text)
@@ -17,24 +26,37 @@ void CustomText::setText(const QString &text)
     }
 }
 
-QSGNode *CustomText::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *)
+void CustomText::setColor(const QColor &color)
 {
-    QQuickTextNode *node = static_cast<QQuickTextNode *>(oldNode);
+    if (m_color != color) {
+        m_color = color;
+        emit colorChanged();
+        update();
+    }
+}
 
-    if (!node) {
-        node = new QQuickTextNode(this);
+QSGNode *CustomText::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *) {
+    QQuickTextNode *textNode = static_cast<QQuickTextNode*>(oldNode);
+    if (!textNode) {
+        textNode = new QQuickTextNode(this);
     }
 
-    // Use fixed properties for simplicity
-    QFont font;
-    font.setPixelSize(20);
-    
-    node->setFont(font);
-    node->setColor(Qt::black);
-    node->setText(m_text);
-    
-    // Position text at (0,0) relative to item
-    node->setPosition(QPointF(0, 0));
+    m_layout = new QTextLayout;
+//m_layout->setFont(myFont);
 
-    return node;
+    // Prepare text layout
+    m_layout->setText(m_text);
+    m_layout->beginLayout();
+    QTextLine line = m_layout->createLine();
+    if (line.isValid()) { 
+        line.setLineWidth(boundingRect().width());
+        line.setPosition(QPointF(0, 0)); 
+        // (create more lines for multi-line text)
+    }
+    m_layout->endLayout();
+    // Clear previous content and add new text
+    textNode->deleteContent();
+    textNode->addTextLayout(QPointF(0, 0), m_layout, m_color);
+    return textNode;
 }
+#endif
--- scratchpad/qt-scenegraph-learnings.md
+++ scratchpad/qt-scenegraph-learnings.md
@@ -1,3 +1,8 @@
 Use some tips for metrics from below code
     https://github.com/KDAB/GammaRay
 
+
+
+Text.
+
+Manual glyph approach: If you absolutely must remain in C++ with no private classes, you can do a partial approach by using a QRawFont to get glyph data, then uploading it to geometry. But that basically reimplements a chunk of QQuickTextNode.
